bf7741f493ad3998667bb78da0eaed9ae7560b22
Georg Wölflein <georgw7777@gmail.com>
2020-10-04 18:25:16 +0100

    Add dummy endpoint

--- server/server.py
+++ server/server.py
@@ -1,10 +1,32 @@
-from typing import Optional
+import typing
+import numpy as np
+import pandas as pd
+import sklearn.metrics
 
 from fastapi import FastAPI
 
 app = FastAPI()
 
+# # Calculate adjacency matrix once on startup
+# df = (pd.read_csv("../data/embeddings.csv")
+#       .drop(columns="Unnamed: 0")
+#       .set_index("V1")
+#       .dropna())
+# adjacency_matrix = sklearn.metrics.pairwise.cosine_similarity(df, df)
+# adjacency_matrix = pd.DataFrame(adjacency_matrix)
+# adjacency_matrix.index = df.index
+# adjacency_matrix.index.name = "id"
+# adjacency_matrix.columns = df.index
 
-@app.get("/")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+# # Read metadata CSV file
+# df = pd.read_csv("../data/data_about_nasa_datasets.csv")
+
+
+@app.post("/recommend")
+def get_recommendation(liked: typing.List[str], disliked: typing.List[str]):
+    return {
+        "id": "abc",
+        "title": "dataset title",
+        "summary": "summ",
+        "url": "http://google.com"
+    }
