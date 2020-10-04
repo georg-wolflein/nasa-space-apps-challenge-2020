636027707ec05beccf2814b5d2b041d18d2f515b
Georg Wölflein <georgw7777@gmail.com>
2020-10-04 18:13:35 +0100

    Data

--- data/get_data.py
+++ data/get_data.py
@@ -4,7 +4,7 @@ import itertools
 import pandas as pd
 import numpy as np
 
-url = "https://cmr.earthdata.nasa.gov/search/collections.json?page_size=1&scroll=true&include_facets=true&hierarchical_facets=true&echo_compatible=true"
+url = "https://cmr.earthdata.nasa.gov/search/collections.json?page_size=100&scroll=true"
 
 
 def _get_data(response):
@@ -12,47 +12,6 @@ def _get_data(response):
         id = dataset["id"]
         title = dataset["title"]
         summary = dataset["summary"]
-
-        print(len(response.json()["feed"]["facets"]))
-        print(len(response.json()["feed"]["entry"]))
-        print("--")
-
-        # let values = []
-        # let topic = resp.data.feed.facets[9].category[0].topic;
-
-        # console.log(topic)
-
-        # //THIS SHIT GETS THE LITTLE KEYWORDS AND NOT THE BIG ONES
-        # for (i = 0; i < topic.length; i++) {
-        #     let terms = topic[i].term
-        #     for (j = 0; j < terms.length; j++) {
-        #         let subfields = terms[j].subfields
-        #         for (x = 0; x < subfields.length; x++) {
-        #             let subValues = terms[j][subfields[x]]
-        #             for (y = 0; y < subValues.length; y++) {
-        #                 values.push(subValues[y].value)
-        #                 subfields2 = subValues[y].subfields
-        #                 if (typeof subfields2 != "undefined") {
-        #                     for (z = 0; z < subfields2.length; z++) {
-        #                         let s2 = subfields2[z]
-        #                         let subfields3 = subValues[y][s2]
-        #                         for (e = 0; e < subfields3.length; e++) {
-        #                             values.push(subfields3[e].value)
-        #                         }
-        #                     }
-        #                 }
-        #             }
-        #         }
-        #     }
-        # }
-
-        # //THIS SHIT GETS THE BIG KEYWORDS
-        # for (i = 0; i < topic.length; i++) {
-        #     values.push(topic[i].value)
-        # }
-
-        # var valuesCSV = values.join('|')
-        # console.log(valuesCSV)
         yield id, title, summary
 
 
@@ -65,13 +24,12 @@ scroll_id = response.headers["CMR-Scroll-Id"]
 data_pages = [get_data(response)]
 
 for i in itertools.count():
-    print("i", i)
+    print(i)
     response = requests.get(url, headers={"CMR-Scroll-Id": scroll_id})
     data = get_data(response)
     if len(data) == 0:
         break
     data_pages.append(data)
-    break
 
 data = np.array(list(itertools.chain.from_iterable(data_pages)))
 df = pd.DataFrame(data)
---
+++ server/README.md
@@ -0,0 +1,19 @@
+# Backend server
+
+## Install
+
+Install the `poetry` dependency manager, and then run
+
+```bash
+poetry install
+```
+
+to install the dependencies for this project
+
+## Run
+
+Run via
+
+```bash
+poetry run ovicorn server:app
+```
